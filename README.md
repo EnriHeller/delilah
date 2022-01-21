10e22be8fac0066fc0120ba897d8ef3d53fc1842
enriheller <enrique.heller@hotmail.com>
2022-01-21 18:39:49 -0300

    inclusión de README.md y el sqlScript.sql

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# delilah
+# Delilah Resto -  REST API
+
+En este proyecto, se desarrolló el backend necesario para un sistema 
+de pedidos online de un restaurante. 
+En el se encuentra definida una REST API la cual permite realizar 
+altas, bajas, modificaciones y obtención de información a una
+base de datos que interrelaciona usuarios 
+(clientes o administradores), platos y pedidos. 
+El desarrollo del mismo parte del objetivo de que sea posible una puesta 
+en producción utilizando web services.   
+
+---
+## Prerequisitos
+---
+Para un montaje del proyecto en el que se permita levantar
+el servidor a nivel local, se tenga acceso directo a la base 
+de datos y sea posible la ejecución de los correspondientes 
+endpoints para interactuar sobre la misma mediante peticiones HTPP, 
+se deberá contar con los siguientes softwares:
+
+* Node JS
+* Git Bash
+* Xampp
+* MySQL Workbench (versión 8.0.26)
+
+---
+## Instalación
+---
+
+Para establecer la conexión a la base de datos, inicializar **Xampp** y encender el módulo de MySQL.
+Luego, inicializar **MySQL Workbench**, establecer una 
+nueva conexión y disponer los siguientes parámetros:
+
+```
+Connection Name: XAMPP MYSQL
+Connextion Method: Standard (TCP/IP)
+Hostname: 127.0.0.1
+Port: 3306
+Username: root 
+```
+Habiendolos confirmado, presionar **OK** e ingresar a la conexión dispuesta.
+Una vez echo esto, abrir el archivo *sqlScript.sql* y ejecutar sus 
+sentencias. Se creará el schema 
+*proyecto-delilah-resto* (para su visualización, se deberá actualizar la lista de schemas). 
+donde se alojarán las tablas correspondientes
+a los tipos de datos con los que se va a interactuar.
+
+Para levantar el servidor a nivel de local, será necesario abrir la carpeta
+del proyecto en la terminal de Git Bash. Luego, se deberá ejecutar `npm install` 
+para instalar las dependencias necesarias, finalmente ejecutar `npm run dev`
+para levantar el servidor y conectarlo a la base de datos.
+
+
+Si los pasos se realizaron correctamente,
+se debería visualizar el siguiente mensaje por consola:
+
+```
+Servidor se ha iniciado en puerto 3000
+Executing (default): SELECT 1+1 AS result
+Conexion exitosa con la db
+```
+
+---
+## Observaciones
+---
+
+* En el caso que se desee correr el servidor en un entorno remoto o
+distinto al definido localmente en este proyecto, en el archivo *.env* se definen las variables de entorno, las cuales pueden modificarse
+sin inconvenientes. Tener en cuenta que también se deberá establecer la conexión en MySQL Workbench.
+
+* Por seguridad, todos los usuarios creados serán no administradores. 
+Para que un usuario sea administrador, ejecutar la siguiente sentencia
+en MySQL Workbench, alojando el ID de dicho usuario:
+
+```
+UPDATE `proyecto-delilah-resto`.`usuarios` SET `esAdmin` = '1' WHERE (`id` = '<id_del_usuario>');
+
+-- ---------------------------------------------------------------------
+-- Ejemplo si el ID es 1:
+-- UPDATE `proyecto-delilah-resto`.`usuarios` SET `esAdmin` = '1' WHERE (`id` = '1');
+```
+
+* Se incluye el archivo *delilah-resto.postman_collection* para
+ser abierto mediante el software **Postman** y ejecutar las
+peticiones HTPP sin necesidad de tener un entorno desarrollado para ello.
+
+* Para visualizar los archivos *spec.yaml* y *spec.json* desde Swagger Editor, es necesario 
+ haber levantado el servidor con el comando `npm run dev`
--- sqlScript.sql
+++ sqlScript.sql
@@ -5,8 +5,13 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema proyecto-delilah-resto
 -- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema proyecto-delilah-resto
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `proyecto-delilah-resto` DEFAULT CHARACTER SET utf8 ;
 -- -----------------------------------------------------
 -- Schema proyecto-delilah-resto
 -- -----------------------------------------------------
@@ -21,7 +26,7 @@ USE `proyecto-delilah-resto` ;
 -- Table `proyecto-delilah-resto`.`usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `proyecto-delilah-resto`.`usuarios` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(300) NOT NULL,
   `usuario` VARCHAR(100) NOT NULL,
   `correo` VARCHAR(150) NOT NULL,
@@ -32,40 +37,15 @@ CREATE TABLE IF NOT EXISTS `proyecto-delilah-resto`.`usuarios` (
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `correo_UNIQUE` (`correo` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `proyecto-delilah-resto`.`pedidos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `proyecto-delilah-resto`.`pedidos` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `precio_total` FLOAT NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
-  `estado` ENUM('NUEVO', 'CONFIRMADO', 'PREPARANDO', 'ENVIANDO', 'CANCELADO', 'ENTREGANDO') NOT NULL DEFAULT 'NUEVO',
-  `forma_pago` ENUM('EF', 'TC', 'MP') NOT NULL,
-  `usuarios_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_pedidos_usuarios_idx` (`usuarios_id` ASC) VISIBLE,
-  CONSTRAINT `fk_pedidos_usuarios`
-    FOREIGN KEY (`usuarios_id`)
-    REFERENCES `proyecto-delilah-resto`.`usuarios` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8;
+  UNIQUE INDEX `correo_UNIQUE` (`correo` ASC))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `proyecto-delilah-resto`.`platos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `proyecto-delilah-resto`.`platos` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(100) NOT NULL,
   `precio` FLOAT NOT NULL,
   `active` TINYINT(1) NOT NULL DEFAULT 1,
@@ -73,23 +53,40 @@ CREATE TABLE IF NOT EXISTS `proyecto-delilah-resto`.`platos` (
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `proyecto-delilah-resto`.`pedidos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `proyecto-delilah-resto`.`pedidos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `precio_total` FLOAT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `estado` ENUM('NUEVO', 'CONFIRMADO', 'PREPARANDO', 'ENVIANDO', 'CANCELADO', 'ENTREGANDO') NOT NULL DEFAULT 'NUEVO',
+  `forma_pago` ENUM('EF', 'TC', 'MP') NOT NULL,
+  `usuarios_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_pedidos_usuarios_idx` (`usuarios_id` ASC),
+  CONSTRAINT `fk_pedidos_usuarios`
+    FOREIGN KEY (`usuarios_id`)
+    REFERENCES `proyecto-delilah-resto`.`usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `proyecto-delilah-resto`.`pedidos_has_platos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `proyecto-delilah-resto`.`pedidos_has_platos` (
-  `pedido_id` INT(11) NOT NULL,
-  `plato_id` INT(11) NOT NULL,
-  `cantidad` INT(11) NOT NULL,
-  `pedidos_id` INT(11) NULL DEFAULT NULL,
-  `platos_id` INT(11) NULL DEFAULT NULL,
+  `pedido_id` INT NOT NULL,
+  `plato_id` INT NOT NULL,
+  `cantidad` INT NOT NULL,
   PRIMARY KEY (`pedido_id`, `plato_id`),
-  INDEX `fk_pedidos_has_platos_platos1_idx` (`plato_id` ASC) VISIBLE,
-  INDEX `fk_pedidos_has_platos_pedidos1_idx` (`pedido_id` ASC) VISIBLE,
+  INDEX `fk_pedidos_has_platos_platos1_idx` (`plato_id` ASC),
+  INDEX `fk_pedidos_has_platos_pedidos1_idx` (`pedido_id` ASC),
   CONSTRAINT `fk_pedidos_has_platos_pedidos1`
     FOREIGN KEY (`pedido_id`)
     REFERENCES `proyecto-delilah-resto`.`pedidos` (`id`)
@@ -100,9 +97,9 @@ CREATE TABLE IF NOT EXISTS `proyecto-delilah-resto`.`pedidos_has_platos` (
     REFERENCES `proyecto-delilah-resto`.`platos` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
+USE `proyecto-delilah-resto` ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
